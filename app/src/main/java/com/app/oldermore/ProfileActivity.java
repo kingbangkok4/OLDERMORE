bab4b5732eadd9b53d08c425b2415e666499194f
oil <kingbangkok4@gmail.com>
2016-12-07 01:00:59 +0700

    Add new

--- app/src/main/java/com/app/oldermore/ProfileActivity.java
+++ app/src/main/java/com/app/oldermore/ProfileActivity.java
@@ -3,12 +3,21 @@ package com.app.oldermore;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.app.oldermore.http.Http;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -18,12 +27,14 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class ProfileActivity extends Activity{
+public class ProfileActivity extends Activity {
     private Double sumTotal = 0.00;
     private StringBuilder strDetailService = new StringBuilder();
     //private DatabaseActivity myDb = new DatabaseActivity(this);
@@ -32,16 +43,20 @@ public class ProfileActivity extends Activity{
     ArrayList<HashMap<String, String>> tmpMyArrList = new ArrayList<HashMap<String, String>>();
     HashMap<String, String> map;
     private Http http = new Http();
-    private  Button btnImageProfile;
+    private ImageButton btnImageProfile;
     private EditText txtName, txtMobile;
-    private String strURLUpload = getString(R.string.url_upload);
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         // Permission StrictMode
-        if (android.os.Build.VERSION.SDK_INT > 9) {
+        if (Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
@@ -57,12 +72,15 @@ public class ProfileActivity extends Activity{
             }
         }
 
-        btnImageProfile = (Button)findViewById(R.id.btnImageProfile);
-        txtName  = (EditText)findViewById(R.id.txtName);
-        txtMobile = (EditText)findViewById(R.id.txtMobile);
+        btnImageProfile = (ImageButton) findViewById(R.id.btnImageProfile);
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtMobile = (EditText) findViewById(R.id.txtMobile);
 
         LoadData();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void LoadData() {
@@ -88,7 +106,7 @@ public class ProfileActivity extends Activity{
                     MyArrProfile.add(map);
 
                     ShowProfile();
-                }else {
+                } else {
                     MessageDialog(error);
                 }
             }
@@ -102,10 +120,26 @@ public class ProfileActivity extends Activity{
     private void ShowProfile() {
         txtName.setText(MyArrProfile.get(0).get("member_name"));
         txtMobile.setText(MyArrProfile.get(0).get("member_mobile"));
+        String photo_url_str = "http://godirvanar.blog.is/users/a2/godirvanar/img/scratchcat-small.png";
+        URL newurl = null;
+        try {
+            newurl = new URL(photo_url_str);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Bitmap b = null;
+        try {
+            b = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        btnImageProfile.setImageBitmap(Bitmap.createScaledBitmap(b, 80, 80, false));
+
+
     }
 
-    public static boolean uploadFiletoServer(String strSDPath, String strUrlServer) {
-
+    public boolean uploadFiletoServer(String strSDPath, String strUrlServer) {
+        String strURLUpload = getString(R.string.url_upload);
         int bytesRead, bytesAvailable, bufferSize;
         byte[] buffer;
         int maxBufferSize = 10 * 1024 * 1024;
@@ -199,4 +233,39 @@ public class ProfileActivity extends Activity{
         alert.show();
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Profile Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
