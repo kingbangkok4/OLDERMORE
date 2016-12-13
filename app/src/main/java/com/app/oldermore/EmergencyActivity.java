9fe8e1838271d744d99b8bacf6136b6e181e96de
kingbangkok4 <kingbangkok4@gmail.com>
2016-12-13 23:30:51 +0700

    dddsadaadad

--- app/src/main/java/com/app/oldermore/EmergencyActivity.java
+++ app/src/main/java/com/app/oldermore/EmergencyActivity.java
@@ -2,14 +2,46 @@ package com.app.oldermore;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.app.oldermore.http.Http;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class EmergencyActivity extends Activity {
@@ -18,8 +50,11 @@ public class EmergencyActivity extends Activity {
     //private DatabaseActivity myDb = new DatabaseActivity(this);
     ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
     ArrayList<HashMap<String, String>> tmpMyArrList = new ArrayList<HashMap<String, String>>();
+    private ArrayList<HashMap<String, String>> ArrListEmergecy = new ArrayList<HashMap<String, String>>();
     HashMap<String, String> map;
     private Http http = new Http();
+    private Button btnAdd, btnMainMenu;
+    private ListView listViewEmergency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +77,258 @@ public class EmergencyActivity extends Activity {
             }
         }
 
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnMainMenu = (Button) findViewById(R.id.btnMainMenu);
+        listViewEmergency = (ListView) findViewById(R.id.listViewEmergency);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogAddEmergency();
+            }
+        });
+        btnMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), MenuActivity.class);
+                i.putExtra("MyArrList", MyArrList);
+                startActivity(i);
+            }
+        });
+
+        LoadData();
+        listViewEmergency.setAdapter(new ImageAdapter(this, ArrListEmergecy));
+
+        listViewEmergency.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                try {
+                    //MyArrList.get(position).get("emergency_id");
+                } catch (Exception e) {
+                    // When Error
+                }
+
+            }
+        });
+
+        listViewEmergency.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                return false;
+            }
+        });
+    }
+
+    private void LoadData() {
+        String url = getString(R.string.url) + "getEmergency.php";
+
+        // Paste Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("user_id", MyArrList.get(0).get("user_id")));
+
+        try {
+            JSONArray data = new JSONArray(http.getJSONUrl(url, params));
+            if (data.length() > 0) {
+                ArrListEmergecy.clear();
+                for (int i = 0; i < data.length(); i++) {
+                    JSONObject c = data.getJSONObject(i);
+                    map = new HashMap<String, String>();
+                    map.put("emergency_id", c.getString("emergency_id"));
+                    map.put("emergency_name", c.getString("emergency_name"));
+                    map.put("emergency_mobile", c.getString("emergency_mobile"));
+                    map.put("emergency_image", c.getString("emergency_image"));
+                    map.put("user_id", c.getString("user_id"));
+                    ArrListEmergecy.add(map);
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+   /* private void SaveData() {
+        String status = "0";
+        String error = "";
+        String url = getString(R.string.url) + "saveEmergency.php";
+        // Paste Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("user_id", MyArrList.get(0).get("user_id")));
+        params.add(new BasicNameValuePair("member_name", txtName.getText().toString().trim()));
+        params.add(new BasicNameValuePair("member_mobile", txtMobile.getText().toString().trim()));
+        params.add(new BasicNameValuePair("user_image", strImgProfile));
+        try {
+            JSONArray data = new JSONArray(http.getJSONUrl(url, params));
+            if (data.length() > 0) {
+                JSONObject c = data.getJSONObject(0);
+                status = c.getString("status");
+                error = c.getString("error");
+                if ("1".equals(status)) {
+                    LoadData();
+                    MessageDialog(error);
+                } else {
+                    MessageDialog(error);
+                }
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            MessageDialog(e.getMessage());
+        }
+    }*/
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+        private ArrayList<HashMap<String, String>> MyArr = new ArrayList<HashMap<String, String>>();
+
+        public ImageAdapter(EmergencyActivity c, ArrayList<HashMap<String, String>> list) {
+            // TODO Auto-generated method stub
+            context = c;
+            MyArr = list;
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return MyArr.size();
+        }
+
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.activity_emergency_column, null);
+            }
+
+            // ColImage
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.ColImgPath);
+            imageView.getLayoutParams().height = 100;
+            imageView.getLayoutParams().width = 100;
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            try {
+                imageView.setImageBitmap(loadBitmap(MyArr.get(position).get("emergency_image")));
+            } catch (Exception e) {
+                // When Error
+                imageView.setImageResource(android.R.drawable.ic_menu_report_image);
+            }
+
+            // ColPosition
+            TextView txtPosition = (TextView) convertView.findViewById(R.id.ColName);
+            txtPosition.setPadding(10, 0, 0, 0);
+            txtPosition.setText("ชื่อ : " + MyArr.get(position).get("emergency_name"));
+
+            // ColPicname
+            TextView txtPicName = (TextView) convertView.findViewById(R.id.ColMobile);
+            txtPicName.setPadding(50, 0, 0, 0);
+            txtPicName.setText("เบอร์โทร : " + MyArr.get(position).get("emergency_mobile"));
+
+            return convertView;
+
+        }
+
+    }
+
+
+    /*****
+     * Get Image Resource from URL (Start)
+     *****/
+    private static final String TAG = "ERROR";
+    private static final int IO_BUFFER_SIZE = 4 * 1024;
+
+    public static Bitmap loadBitmap(String url) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+        BufferedOutputStream out = null;
+
+        try {
+            in = new BufferedInputStream(new URL(url).openStream(), IO_BUFFER_SIZE);
+
+            final ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+            out = new BufferedOutputStream(dataStream, IO_BUFFER_SIZE);
+            copy(in, out);
+            out.flush();
+
+            final byte[] data = dataStream.toByteArray();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            //options.inSampleSize = 1;
+
+            bitmap = BitmapFactory.decodeByteArray(data, 0, data.length, options);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not load Bitmap from: " + url);
+        } finally {
+            closeStream(in);
+            closeStream(out);
+        }
+
+        return bitmap;
+    }
+
+    private static void closeStream(Closeable stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                android.util.Log.e(TAG, "Could not close stream", e);
+            }
+        }
+    }
+
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] b = new byte[IO_BUFFER_SIZE];
+        int read;
+        while ((read = in.read(b)) != -1) {
+            out.write(b, 0, read);
+        }
+    }
+
+    /*****
+     * Get Image Resource from URL (End)
+     *****/
+
+    private void DialogAddEmergency() {
+        View dialogBoxView = View.inflate(this, R.layout.dialog_add_emergency, null);
+        final Button btnSave = (Button) dialogBoxView.findViewById(R.id.btnSave);
+        final ImageButton btnImageProfile = (ImageButton) dialogBoxView.findViewById(R.id.btnImageProfile);
+        final EditText txtName = (EditText) dialogBoxView.findViewById(R.id.txtName);
+        final EditText txtMobile = (EditText) dialogBoxView.findViewById(R.id.txtMobile);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        AlertDialog.Builder builderInOut = new AlertDialog.Builder(this);
+        builderInOut.setTitle("Items Management");
+        builderInOut.setMessage("เพิ้ม - แก้ไข")
+                .setView(dialogBoxView)
+                .setCancelable(false)
+       /*         .setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })*/
+                .setNegativeButton("ปิด",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).show();
 
     }
 
