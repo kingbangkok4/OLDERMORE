0a9cd3566db30988a75f05cb6062ca37ab62dad2
kingbangkok4 <kingbangkok4@gmail.com>
2016-12-23 22:01:05 +0700

    dddsadaadad

--- app/src/main/java/com/app/oldermore/MapActivity.java
+++ app/src/main/java/com/app/oldermore/MapActivity.java
@@ -1,18 +1,17 @@
 package com.app.oldermore;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.v4.app.ActivityCompat;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -21,7 +20,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MapActivity extends FragmentActivity implements LocationListener {
-
+    GPSTracker gps = new GPSTracker(this);
+    private static Location TODO;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
 
@@ -29,11 +29,11 @@ public class MapActivity extends FragmentActivity implements LocationListener {
     private GoogleMap googleMap;
 
     // Latitude & Longitude
-    private Double Latitude = 0.00;
-    private Double Longitude = 0.00;
+    private Double Latitude=0.00;
+    private Double Longitude=0.00;
 
-    ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
-    ArrayList<HashMap<String, String>> tmpMyArrList = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> MyArrList=new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> tmpMyArrList=new ArrayList<HashMap<String, String>>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,44 +41,54 @@ public class MapActivity extends FragmentActivity implements LocationListener {
 
         // Permission StrictMode
         if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+            StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder()
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
 
-        Bundle extras = getIntent().getExtras();
+        Bundle extras=getIntent().getExtras();
         // เช็คว่ามีค่าที่ส่งมาจากหน้าอื่นหรือไม่ถ้ามีจะไม่เท่ากับ null
         if (extras != null) {
-            tmpMyArrList = (ArrayList<HashMap<String, String>>) extras
+            tmpMyArrList=(ArrayList<HashMap<String, String>>) extras
                     .getSerializable("MyArrList");
             if (tmpMyArrList != null) {
-                MyArrList = tmpMyArrList;
+                MyArrList=tmpMyArrList;
             }
         }
 
         //*** Location
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
+        //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         //*** Display Google Map
-        googleMap = ((SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
-        //For test Location
-        Location location= new Location("");
-        location.setLatitude(13.756331);
-        location.setLongitude(100.501765);
+        /*googleMap = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();*/
 
-        onLocationChanged(location);
+       /* Criteria criteria = new Criteria();
+        String mprovider;
+
+        mprovider = locationManager.getBestProvider(criteria, false);
+        if (mprovider != null && !mprovider.equals("")) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(mprovider, 15000, 1, this);
+            Location location = locationManager.getLastKnownLocation(mprovider);
+            if (location != null)
+                onLocationChanged(location);
+            else
+                Toast.makeText(getBaseContext(), "ไม่พบสถานที่ของคุณ กรุณาเปิด GPS!", Toast.LENGTH_SHORT).show();
+        }*/
+
+
+        Location location=new Location("");
+        //*** For test Location
+      /*  location.setLatitude(13.756331);
+        location.setLongitude(100.501765);*/
+        //*** Data Using
+        // location.setLatitude(location.getLatitude());
+        // location.setLongitude(location.getLongitude());
+        location = gps.getLocation();
+         onLocationChanged(location);
     }
 
     @Override
@@ -88,37 +98,50 @@ public class MapActivity extends FragmentActivity implements LocationListener {
             return;
         }
 
+        String msg="New Latitude: " + location.getLatitude()
+                + "New Longitude: " + location.getLongitude();
+
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+
         //** Get Latitude & Longitude
-        Latitude = location.getLatitude();
-        Longitude = location.getLongitude();
+        Latitude=location.getLatitude();
+        Longitude=location.getLongitude();
 
         googleMap.clear();
 
         //*** Focus & Zoom
-        LatLng coordinate = new LatLng(Latitude, Longitude);
+        LatLng coordinate=new LatLng(Latitude, Longitude);
         googleMap.setMapType(com.google.android.gms.maps.GoogleMap.MAP_TYPE_HYBRID);
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 17));
 
         //*** Marker
-        MarkerOptions marker = new MarkerOptions()
-                .position(new LatLng(Latitude, Longitude)).title("Your current location");
+        MarkerOptions marker=new MarkerOptions()
+                .position(new LatLng(Latitude, Longitude)).title("คุณอยู่ตรงนี้");
         marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
         googleMap.addMarker(marker);
     }
 
+
     @Override
     public void onProviderDisabled(String provider) {
         // Log.d("Latitude","disable");
+        Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onProviderEnabled(String provider) {
         // Log.d("Latitude","enable");
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // Log.d("Latitude","status");
+        // TODO Auto-generated method stub
     }
 
 }
--- app/src/main/java/com/app/oldermore/MapsActivity.java
+++ app/src/main/java/com/app/oldermore/MapsActivity.java
@@ -1,5 +1,6 @@
-package com.app.oldermore;
 
+package com.app.oldermore;
+/*
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -25,7 +26,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
+    */
+/**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
@@ -33,7 +35,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
-     */
+     *//*
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -44,3 +47,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
+*/
