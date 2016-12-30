6295397068502ac409b4621a37ca8b1354315f19
kingbangkok4 <kingbangkok4@gmail.com>
2016-12-30 18:19:35 +0700

    dddsadaadad

--- app/src/main/java/com/app/oldermore/PostActivity.java
+++ app/src/main/java/com/app/oldermore/PostActivity.java
@@ -13,20 +13,27 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.app.oldermore.R;
 import com.app.oldermore.http.Http;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class PostActivity extends Activity {
     private Double sumTotal = 0.00;
     private StringBuilder strDetailService = new StringBuilder();
     //private DatabaseActivity myDb = new DatabaseActivity(this);
+    private ArrayList<HashMap<String, String>> MyArrProfile = new ArrayList<HashMap<String, String>>();
     ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
     ArrayList<HashMap<String, String>> tmpMyArrList = new ArrayList<HashMap<String, String>>();
     HashMap<String, String> map;
@@ -64,7 +71,7 @@ public class PostActivity extends Activity {
         btnImageProfile = (ImageButton) findViewById(R.id.btnImageProfile);
         lblName = (TextView) findViewById(R.id.lblName);
 
-        ShowProfile();
+        LoadData();
 
         btnMainMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,11 +102,44 @@ public class PostActivity extends Activity {
         });
     }
 
+    private void LoadData() {
+        MyArrProfile = new ArrayList<HashMap<String, String>>();
+        String status = "0";
+        String error = "";
+        String url = getString(R.string.url) + "getMember.php";
+        // Paste Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("user_id", MyArrList.get(0).get("user_id")));
+        try {
+            JSONArray data = new JSONArray(http.getJSONUrl(url, params));
+            if (data.length() > 0) {
+                JSONObject c = data.getJSONObject(0);
+                status = c.getString("status");
+                error = c.getString("error");
+                //if ("1".equals(status)) {
+                map = new HashMap<String, String>();
+                map.put("member_id", c.getString("member_id"));
+                map.put("member_name", c.getString("member_name"));
+                map.put("member_mobile", c.getString("member_mobile"));
+                map.put("member_address", c.getString("member_address"));
+                map.put("member_email", c.getString("member_email"));
+                map.put("user_image", c.getString("user_image"));
+                MyArrProfile.add(map);
+
+                ShowProfile();
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            MessageDialog(e.getMessage());
+        }
+    }
+
     private void ShowProfile() {
-        String photo = MyArrList.get(0).get("user_image");
+        lblName.setText(MyArrProfile.get(0).get("member_name"));
         String photo_url_str = getString(R.string.url_images);
-        if (!"".equals(photo) && photo != null) {
-            photo_url_str += photo;
+        if (!"".equals(MyArrProfile.get(0).get("user_image")) && MyArrProfile.get(0).get("user_image") != null) {
+            photo_url_str += MyArrProfile.get(0).get("user_image");
         } else {
             photo_url_str += "no.png";
         }
@@ -116,7 +156,6 @@ public class PostActivity extends Activity {
             e.printStackTrace();
         }
         btnImageProfile.setImageBitmap(Bitmap.createScaledBitmap(b, 80, 80, false));
-        lblName.setText(MyArrList.get(0).get("username"));
     }
 
     private void MessageDialog(String msg) {
