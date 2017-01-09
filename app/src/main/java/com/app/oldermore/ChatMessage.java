ce50a29325766d8530bbead516440f3593287c0f
kingbangkok4 <kingbangkok4@gmail.com>
2017-01-09 15:35:37 +0700

    dddsadaadad

---
+++ app/src/main/java/com/app/oldermore/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.app.oldermore;
+
+/**
+ * Created by Administrator on 1/8/2017.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left , String message) {
+        // TODO Auto-generated constructor stub
+        super();
+        this.left=left;
+        this.message = message;
+    }
+}
