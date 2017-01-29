23267f2e109ae9251350434a15c241f96cdff788
oil <kingbangkok4@gmail.com>
2017-01-30 00:29:15 +0700

    Add Alarm Alert

--- app/src/main/java/com/app/oldermore/MapActivity.java
+++ app/src/main/java/com/app/oldermore/MapActivity.java
@@ -1,17 +1,22 @@
 package com.app.oldermore;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -29,11 +34,11 @@ public class MapActivity extends FragmentActivity implements LocationListener {
     private GoogleMap googleMap;
 
     // Latitude & Longitude
-    private Double Latitude=0.00;
-    private Double Longitude=0.00;
+    private Double Latitude = 0.00;
+    private Double Longitude = 0.00;
 
-    ArrayList<HashMap<String, String>> MyArrList=new ArrayList<HashMap<String, String>>();
-    ArrayList<HashMap<String, String>> tmpMyArrList=new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> tmpMyArrList = new ArrayList<HashMap<String, String>>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,30 +46,40 @@ public class MapActivity extends FragmentActivity implements LocationListener {
 
         // Permission StrictMode
         if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder()
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
 
-        Bundle extras=getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
         // เช็คว่ามีค่าที่ส่งมาจากหน้าอื่นหรือไม่ถ้ามีจะไม่เท่ากับ null
         if (extras != null) {
-            tmpMyArrList=(ArrayList<HashMap<String, String>>) extras
+            tmpMyArrList = (ArrayList<HashMap<String, String>>) extras
                     .getSerializable("MyArrList");
             if (tmpMyArrList != null) {
-                MyArrList=tmpMyArrList;
+                MyArrList = tmpMyArrList;
             }
         }
 
         //*** Location
-        //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         //*** Display Google Map
-        /*googleMap = ((SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map)).getMap();*/
+        googleMap = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
 
        /* Criteria criteria = new Criteria();
-        String mprovider;
+/*        String mprovider;
 
         mprovider = locationManager.getBestProvider(criteria, false);
         if (mprovider != null && !mprovider.equals("")) {
