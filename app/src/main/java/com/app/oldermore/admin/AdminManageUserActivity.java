7455b9072e22983f3a56f85a8229a991260bc4c0
kingbangkok4 <kingbangkok4@gmail.com>
2017-04-18 15:23:05 +0700

    MA_User

--- app/src/main/java/com/app/oldermore/admin/AdminManageUserActivity.java
+++ app/src/main/java/com/app/oldermore/admin/AdminManageUserActivity.java
@@ -1,14 +1,18 @@
 package com.app.oldermore.admin;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +21,7 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -37,6 +42,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -57,7 +63,7 @@ public class AdminManageUserActivity extends Activity {
     private ListView listView;
     private EditText txtSearch;
     private String friendName = "";
-    private String friendUserId = "";
+    private String friendUserId = "", strImgProfile = "";
     private String nameSearch = "";
 
     @Override
@@ -107,6 +113,14 @@ public class AdminManageUserActivity extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
+                try {
+                    //int emId = Integer.parseInt(ArrListFreind.get(position).get("emergency_id"));
+                    DialogAddEmergency(true, position);
+                } catch (Exception e) {
+                    // When Error
+                    MessageDialog(e.getMessage());
+                }
+
                /* String friendId = ArrListFC.get(position).get("friend_id");
                 String friendName = ArrListFC.get(position).get("member_name");
                 Intent i = new Intent(getBaseContext(), ChatActivity.class);
@@ -140,6 +154,137 @@ public class AdminManageUserActivity extends Activity {
             }
         });
 
+    }
+
+    private void DialogAddEmergency(Boolean editMode, int position) {
+        View dialogBoxView = View.inflate(this, R.layout.dialog_add_emergency, null);
+        String strImgProfile = "";
+        final Button btnSave = (Button) dialogBoxView.findViewById(R.id.btnAdd);
+        final Button btnCall = (Button) dialogBoxView.findViewById(R.id.btnCall);
+        final ImageButton btnImageProfile = (ImageButton) dialogBoxView.findViewById(R.id.btnImageProfile);
+        final EditText txtName = (EditText) dialogBoxView.findViewById(R.id.txtName);
+        final EditText txtMobile = (EditText) dialogBoxView.findViewById(R.id.txtMobile);
+        final String[] name = {""};
+        final String[] mobile = {""};
+        String member_id = "";
+        //String user_id = "";
+
+        if (editMode) {
+            member_id = ArrListFriend.get(position).get("member_id");
+            name[0] = ArrListFriend.get(position).get("member_name");
+            mobile[0] = ArrListFriend.get(position).get("member_mobile");
+            //user_id = MyArrList.get(0).get("user_id");
+
+            String photo_url_str = getString(R.string.url_images);
+            if (!"".equals(ArrListFriend.get(position).get("user_image")) && ArrListFriend.get(position).get("user_image") != null) {
+                photo_url_str += ArrListFriend.get(position).get("user_image");
+            } else {
+                photo_url_str += "no.png";
+            }
+            URL newurl = null;
+            try {
+                newurl = new URL(photo_url_str);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            Bitmap b = null;
+            try {
+                b = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            btnImageProfile.setImageBitmap(Bitmap.createScaledBitmap(b, 80, 80, false));
+
+            txtName.setText(name[0]);
+            txtMobile.setText(mobile[0]);
+        } else {
+            member_id = "";
+            name[0] = txtName.getText().toString().trim();
+            mobile[0] = txtMobile.getText().toString().trim();
+            //user_id = MyArrList.get(0).get("user_id");
+        }
+
+        final String finalMember_id = member_id;
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!"".equals(txtMobile.getText().toString().trim())) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + txtMobile.getText().toString().trim()));
+
+                    if (ActivityCompat.checkSelfPermission(getBaseContext(),
+                            Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    startActivity(callIntent);
+                }
+            }
+        });
+        //final String finalUser_id = user_id;
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name[0] = txtName.getText().toString().trim();
+                mobile[0] = txtMobile.getText().toString().trim();
+
+                if (!"".equals(name[0]) && !"".equals(mobile[0])) {
+                    SaveData(finalMember_id, name[0], mobile[0]);
+
+                    LoadDataFriend();
+                } else {
+                    MessageDialog("กรุณาใส่ข้อมูลให้ครบถ้วน");
+                }
+            }
+        });
+
+        AlertDialog.Builder builderInOut = new AlertDialog.Builder(this);
+        builderInOut.setTitle("เพิ่ม - แก้ไข ผู้ติดต่อฉุกเฉิน");
+        builderInOut.setMessage("")
+                .setView(dialogBoxView)
+                .setCancelable(false)
+       /*         .setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })*/
+                .setNegativeButton("ปิด",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).show();
+    }
+
+    private void SaveData(String emergency_id, String member_name, String member_mobile) {
+        String status = "0";
+        String error = "";
+        String url = getString(R.string.url) + "saveEmergency.php";
+        // Paste Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("member_id", emergency_id));
+        params.add(new BasicNameValuePair("user_id", MyArrList.get(0).get("user_id")));
+        params.add(new BasicNameValuePair("member_name", member_name));
+        params.add(new BasicNameValuePair("member_mobile", member_mobile));
+        params.add(new BasicNameValuePair("member_image", strImgProfile));
+        try {
+            JSONArray data = new JSONArray(http.getJSONUrl(url, params));
+            if (data.length() > 0) {
+                JSONObject c = data.getJSONObject(0);
+                status = c.getString("status");
+                error = c.getString("error");
+                if ("1".equals(status)) {
+                    LoadDataFriend();
+                    MessageDialog(error);
+                } else {
+                    MessageDialog(error);
+                }
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            MessageDialog(e.getMessage());
+        }
     }
 
     private void DeleteUser() {
