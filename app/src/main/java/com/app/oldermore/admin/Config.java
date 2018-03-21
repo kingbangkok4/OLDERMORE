65bb447edbe98d294c4bae9ca0db2b73713d1250
oil <kingbangkok4@gmail.com>
2018-03-21 22:29:57 +0700

    oldermore.thaisoftwareretail.com

--- app/src/main/java/com/app/oldermore/admin/Config.java
+++ app/src/main/java/com/app/oldermore/admin/Config.java
@@ -1,7 +1,7 @@
 package com.app.oldermore.admin;
 
 public class Config {
-    public static final String UPLOAD_URL_VIDEO= "http://oldermore.tpakrsai.com/api/uploadVideo.php";
+    public static final String UPLOAD_URL_VIDEO= "http://oldermore.thaisoftwareretail.com/api/uploadVideo.php";
 
     public static final String TAG_URL = "url";
     public static final String TAG_SIZE = "size";
